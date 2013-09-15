e8ad1061a1a3007d7c11c364ff90c85272e72c2e
Irene <irenesanznieto@gmail.com>
2013-09-15 12:56:24 +0200

    basic video capture in opencv

--- src/apps/fingers.cpp
+++ src/apps/fingers.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
+
 using namespace std; 
 
 
 int main () 
 {
+	cv::Mat image; 
+	cv::VideoCapture cap(0); 
+
+   if (!cap.isOpened())
+        cerr << "Cannot open video device" << endl;
 
 
+	while (1) 
+	{
+		cap>>image;
+		cv :: imshow ("VIDEO" , image); 
 
+		if (cv::waitKey(1)== 27)		//press ESC to exit camera
+			break;          
+	} 	                             
+   
+	return 0;
 }
