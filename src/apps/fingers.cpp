6773a2a6d4d6230f2276b9ca6c20562a9b574c1c
Irene <irenesanznieto@gmail.com>
2013-09-16 18:00:55 +0200

    backup - working on background substractor

--- src/apps/fingers.cpp
+++ src/apps/fingers.cpp
@@ -3,11 +3,13 @@
 
 using namespace std; 
 
-void contours (cv:: Mat &);
+void contours (cv:: Mat &, cv:: Mat &);
+void background_substractor (cv:: Mat &, cv:: Mat &);
+
 
 int main () 
 {
-	cv::Mat image; 
+	cv::Mat image, no_back_image, contour_image; 
 	cv::VideoCapture cap(0); 
 
    if (!cap.isOpened())
@@ -17,9 +19,19 @@ int main ()
 	while (1) 
 	{
 		cap>>image;
-		//cv::imshow ("VIDEO" , image); 
+		cvtColor( image, image, CV_BGR2GRAY );
+		cv::flip(image,image,1);
 
-		contours (image); 
+		
+		background_substractor(image, no_back_image); 
+		//contours (image); 
+
+
+		
+		//SHOW THE DIFFERENT IMAGES
+		cv::imshow ("ORIGINAL IMAGE" , image); 
+		//cv::imshow ("CONVEX HULL" , convex_image); 
+
 
 		if (cv::waitKey(1)== 27)		//press ESC to exit camera
 			break;          
@@ -28,10 +40,16 @@ int main ()
 	return 0;
 }
 
-void contours (cv::Mat &src)
+void background_substractor (cv:: Mat &src, cv:: Mat & result)
 {
 
-	cv::Mat src_gray;
+
+
+}
+
+void contours (cv::Mat &src, cv :: Mat & result)
+{
+
 	int thresh = 100;
 	int max_thresh = 255;
 
@@ -41,12 +59,12 @@ void contours (cv::Mat &src)
 	vector<vector<cv::Point> > contours;
 	vector<cv::Vec4i> hierarchy;
 
-	cvtColor( src, src_gray, CV_BGR2GRAY );
- 	blur( src_gray, src_gray, cv::Size(3,3) );
+
+ 	//blur( src_gray, src_gray, cv::Size(3,3) );
 
 	/// Detect edges using canny
 
-	cv::Canny( src_gray, canny_output, thresh, thresh*2 );
+	cv::Canny( src, canny_output, thresh, thresh*2 );
 
 
 
@@ -54,37 +72,11 @@ void contours (cv::Mat &src)
 
 	cv::findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
 
-	cv::Mat drawing = cv::Mat::zeros( canny_output.size(), CV_8UC3 );
-
-	 
-	/// Draw the rectangle that contains the hand 
-	
-	cv:: rectangle (drawing, cv:: Point (150,150), cv:: Point(400,450), cv:: Scalar (0,0,255), 5); 
-
-	/// Choose the contours within the rectangle
-
-
-/*	WORKING ON THIS!
-
-	if (cv::waitKey(1)==32)
-	{
-	while(contours)
- 	{
-    	result = cvApproxPoly(contours, sizeof(CvContour), storage, CV_POLY_APPROX_DP, cvContourPerimeter(contours)*0.02, 0);
-    }
-
-	}
-
-*/
-
+	result= cv::Mat::zeros( canny_output.size(), CV_8UC3 );
 
 	/// Draw the contours
 
 	for( int i = 0; i< contours.size(); i++ )
-		   cv::drawContours( drawing, contours, i, cv:: Scalar (255,255,255), 2, 8, hierarchy, 0, cv::Point() );
-
-	/// Show the results
-
-	cv::imshow( "Contours", drawing );
+		   cv::drawContours( result, contours, i, cv:: Scalar (255,255,255), 2, 8, hierarchy, 0, cv::Point() );
 
 }
