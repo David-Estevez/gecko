3651c0a592c9f6d07293047bb0ec72dcbc1fb1d0
David-Estevez <david.estevez.fdez@gmail.com>
2013-09-23 22:22:50 +0200

    Filter by color. Red is not working yet.

--- src/apps/hand.cpp
+++ src/apps/hand.cpp
@@ -3,9 +3,9 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include "../libraries/HandDetector.h"
 
-void processFrame(cv::Mat &src, cv::Mat &dst, int debug  = 0);
-void drawCalibrationMarks(cv::Mat &input, int halfSide = 20,  cv::Scalar color  = cv::Scalar( 0, 255, 0 ) );
-int getSkinHueValue( cv::Mat &hue_image, int& result,  int halfSide = 20 );
+void processFrame(cv::Mat &src, cv::Mat &dst, int& thresholdValue, int debug  = 0);
+void drawCalibrationMarks(cv::Mat& input, cv::Mat& output, int halfSide = 20,  cv::Scalar color  = cv::Scalar( 0, 255, 0 ) );
+int getSkinHueValue( cv::Mat &hue_image, int& result);
 void getHue( cv::Mat& src, cv::Mat& hue);
 
 int main( int argc, char * argv[] )
@@ -58,15 +58,16 @@ int main( int argc, char * argv[] )
     while( !stop)
     {
 	//-- Get current frame
-	cv::Mat frame;
+	cv::Mat frame, cal_screen;
 	if (! cap.read( frame ) )
 	    break;
+	cv::flip(frame,frame,1);
 
 	//-- Add calibration frame
-	drawCalibrationMarks(frame, halfSide);
+	drawCalibrationMarks(frame, cal_screen, halfSide);
 
 	//-- Show calibration screen
-	cv::imshow( "Calibrating skin", frame );
+	cv::imshow( "Calibrating skin", cal_screen);
 
 	//-- Wait for user confirmation
 	char key =  cv::waitKey(delay);
@@ -96,10 +97,11 @@ int main( int argc, char * argv[] )
 	cv::Mat frame;
 	if ( ! cap.read( frame ) )
 	    break;
+	cv::flip(frame,frame,1);
 
 	//-- Process it
-	cv::Mat processed = frame;
-	processFrame( frame, processed, debugValue);
+	cv::Mat processed = frame.clone();
+	processFrame( frame, processed, skinValue, debugValue);
 
 	//-- Show processed image
 	cv::imshow( "Processed Stream", processed);
@@ -127,7 +129,7 @@ return 0;
 }
 
 
-void processFrame( cv::Mat& src, cv::Mat& dst, int debug)
+void processFrame( cv::Mat& src, cv::Mat& dst, int& thresholdValue, int debug)
 {
 
     if (debug == 0 )
@@ -135,33 +137,67 @@ void processFrame( cv::Mat& src, cv::Mat& dst, int debug)
 	//canny( src, dst);
 	//substractor(src, dst);
 
-	//-- Obtain hue channel:
-	cv::Mat hue;
-	getHue( src, hue);
 
-	cv::Mat thresh;
-	cv::threshold( hue, thresh, 45, 255, cv::THRESH_BINARY);
-	//cv::inRange(hsv, Scalar(0, 58, 89), Scalar(25, 173, 229), bw);
+	//-- Obtain HSV channels:
+	//------------------------------------------------------------------------------------------------
+	cv::Mat srcHSV;
+	cv::cvtColor( src, srcHSV, CV_BGR2HSV);
+
+	std::vector<cv::Mat> hsv;
+	cv::split( srcHSV, hsv);
+	cv::Mat hue = hsv[0], sat = hsv[1], val = hsv[2];
 
 
-	dst = thresh;
+	//-- Threshold channels:
+	//------------------------------------------------------------------------------------------------
+	cv::Mat hueThresh, satThresh, valThresh;
+
+	//-- Hue
+	int interval = 10;
+	cv::inRange( hue, thresholdValue-interval/2 < 0 ? 180-thresholdValue : thresholdValue-interval/2,
+			      thresholdValue+interval/2 > 180 ? thresholdValue : thresholdValue+interval/2,
+			      hueThresh);
+
+	cv::imshow( "Hue", hueThresh);
+
+	//-- Saturation
+	cv::threshold( sat, satThresh, 30, 0, cv::THRESH_TOZERO);
+	cv::imshow( "Sat", satThresh);
+
+	//-- Value
+	cv::threshold( val, valThresh, 60, 0, cv::THRESH_TOZERO);
+	cv::imshow( "Val", valThresh);
+
+
+	//-- Joint channels and convert back to BGR
+	//------------------------------------------------------------------------------------------------
+	std::vector<cv::Mat> hsvThresh;
+	hsvThresh.push_back(hueThresh);
+	hsvThresh.push_back(satThresh);
+	hsvThresh.push_back(valThresh);
+
+	cv::merge( hsvThresh, dst);
+	cv::cvtColor( dst, dst, CV_HSV2BGR);
+
+	//dst = satThresh;
     }
     else if (debug == -1)
     {
-	//-- Do nothing (is this possible?)
+	//-- Do nothing
 	return;
     }
 
 }
 
-void drawCalibrationMarks( cv::Mat& input, int halfSide, cv::Scalar color)
+void drawCalibrationMarks( cv::Mat& input, cv::Mat& output, int halfSide, cv::Scalar color)
 {
     //-- Get image dimensions
     int image_cols = input.cols;
     int image_rows = input.rows;
 
     //-- Draw a rectangle in the output image
-    cv::rectangle( input,
+    output = input.clone();
+    cv::rectangle( output,
 		   cv::Point( image_cols / 2 - halfSide,  image_rows/2 - halfSide ),
 		   cv::Point( image_cols / 2 + halfSide,  image_rows/2 + halfSide),
 		   color
@@ -181,7 +217,7 @@ void getHue( cv::Mat& src, cv::Mat& hue)
 
 }
 
-int getSkinHueValue( cv::Mat& ROI , int& result,  int halfSide )
+int getSkinHueValue( cv::Mat& ROI , int& result )
 {
     //-- Obtain hue channel:
     cv::Mat hue;
@@ -192,10 +228,8 @@ int getSkinHueValue( cv::Mat& ROI , int& result,  int halfSide )
 
     for (int i = 0; i < hue.cols; i++ )
 	for( int j = 0; j < hue.rows; j++)
-	{   std::cout << hue.at<unsigned int>(i, j) << std::endl;
-	    average += hue.at<unsigned int>(i, j);
-}
-    average /= (hue.cols * hue.rows);
+	    average += (int) hue.at<unsigned char>(i, j);
 
+    average /= (hue.cols * hue.rows);
     result = average;
 }
