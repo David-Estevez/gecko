40f6fa376234ef60ade9d655e2d466ca714d22fb
David-Estevez <david.estevez.fdez@gmail.com>
2013-10-23 17:29:44 +0200

    Kalman filter added for hand angle.

--- src/apps/hand.cpp
+++ src/apps/hand.cpp
@@ -86,6 +86,27 @@ int main( int argc, char * argv[] )
 
 
 
+    //-- Kalman filter setup for estimating hand angle:
+    //-----------------------------------------------------------------------
+    //-- Create filter:
+    cv::KalmanFilter kalmanFilterAngle( 2, 1, 0);
+    kalmanFilterAngle.transitionMatrix = *( cv::Mat_<float>(2, 2) << 1, 1,
+								     0, 1);
+
+    //-- Create matrix for storing the measurement (measured position of hand)
+    cv::Mat_<float> angleMeasurement(1, 1);
+    angleMeasurement.setTo( cv::Scalar(0));
+
+    //-- Initial state:
+    kalmanFilterAngle.statePre.at<float>(0) = 90; //-- initial angle
+    kalmanFilterAngle.statePre.at<float>(1) = 0;  //-- initial angular velocity
+
+    //-- Set the rest of the matrices:
+    cv::setIdentity( kalmanFilterAngle.measurementMatrix );
+    cv::setIdentity( kalmanFilterAngle.processNoiseCov, cv::Scalar::all(0.0001));
+    cv::setIdentity( kalmanFilterAngle.measurementNoiseCov, cv::Scalar::all(0.1));
+    cv::setIdentity( kalmanFilterAngle.errorCovPost, cv::Scalar::all(0.1));
+
 
     //-- Calibration loop
     //--------------------------------------------------------------------
@@ -249,7 +270,7 @@ int main( int argc, char * argv[] )
 		cv::Point predictedPoint( prediction.at<float>(0), prediction.at<float>(1) );
 
 		//-- (Optional) Print predicted point on screen:
-		cv::circle( display, predictedPoint, 3, cv::Scalar( 0, 255, 0), 2 );
+		cv::circle( display, predictedPoint, 4, cv::Scalar( 0, 255, 0), 2 );
 
 		//-- Measure actual point (uncomment the selected method):
 		int cogX = 0, cogY = 0;
@@ -287,7 +308,7 @@ int main( int argc, char * argv[] )
 		cv::Point estimationPoint( estimation.at<float>(0), estimation.at<float>(1) );
 
 		//- (Optional) Print estimation on screen:
-		cv::circle( display, estimationPoint, 4, cv::Scalar( 0, 0, 255), 2 );
+		cv::circle( display, estimationPoint, 3, cv::Scalar( 0, 0, 255), 2 );
 
 		//-- Get screen dimensions:
 		int screenHeight, screenWidth;
@@ -314,14 +335,44 @@ int main( int argc, char * argv[] )
 	//-----------------------------------------------------------------------------------------------------
 	//-- Print "Angle control"
 	//-----------------------------------------------------------------------------------------------------
+	//-- Matrix that shows the gauge
 	cv::Mat gauge = cv::Mat::zeros( 100, 200, CV_8UC3);
 
+	//-- Predict angle with Kalman filter:
+	cv::Mat anglePrediction = kalmanFilterAngle.predict();
+
+	//-- Measure:
+	angleMeasurement(0) = handAngle;
+
+	//-- Correct prediction:
+	cv::Mat angleEstimation = kalmanFilterAngle.correct( angleMeasurement );
+
+	//-- Define gauge:
 	int gauge_l = 60;
+	cv::Point gaugeOrigin( 200/2, 80 );
+
+	//-- Calculate actual end:
 	double rad_ang = handAngle * 3.1415 / 180.0;
 	int x_coord = gauge_l * cos( rad_ang);
 	int y_coord = gauge_l * sin( rad_ang);
+	cv::Point gaugeEnd( 200/2 + x_coord , 80 - y_coord);
 
-	cv::line( gauge, cv::Point( 200/2, 80 ), cv::Point( 200/2 + x_coord , 80 - y_coord), cv::Scalar( 0, 0, 255));
+	//-- Calculate predicted end:
+	double rad_ang_predicted = anglePrediction.at<float>(0) * 3.1415 / 180.0;
+	int x_coord_predicted = gauge_l * cos( rad_ang_predicted);
+	int y_coord_predicted = gauge_l * sin( rad_ang_predicted);
+	cv::Point predictedGaugeEnd( 200/2 + x_coord_predicted , 80 - y_coord_predicted);
+
+	//-- Calculate estimated end:
+	double rad_ang_estimated = angleEstimation.at<float>(0) * 3.1415 / 180.0;
+	int x_coord_estimated = gauge_l * cos( rad_ang_estimated);
+	int y_coord_estimated = gauge_l * sin( rad_ang_estimated);
+	cv::Point estimatedGaugeEnd( 200/2 + x_coord_estimated , 80 - y_coord_estimated);
+
+	//-- Show the gauge(s):
+	cv::line( gauge, gaugeOrigin, gaugeEnd, cv::Scalar( 255, 0, 0), 1); //-- Actual angle
+	cv::line( gauge, gaugeOrigin, predictedGaugeEnd, cv::Scalar( 0, 255, 0), 2); //-- Predicted angle
+	cv::line( gauge, gaugeOrigin, estimatedGaugeEnd, cv::Scalar( 0, 0, 255)), 3; //-- Estimated angle
 	cv::imshow( "Gauge", gauge);
 
 	//-----------------------------------------------------------------------------------------------------
